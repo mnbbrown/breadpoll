8ab5a9a295179bbde309ac9450d696b2c236c2a3
Mike Monteith <mike@mikemonteith.com>
2020-04-20 21:54:20 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.shipton-mill.com/queue"
+exact_match = "Sorry, we don't have any delivery slots available at this time, please come back later."
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+sorry_messages = soup.body.find_all(text=exact_match)
+if len(sorry_messages) == 0:
+    # There is no sorry message! Alert me!
+    print("SEND ALERT")
+else:
+    print("NO ALERT")
