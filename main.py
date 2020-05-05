322f291239af0d1d9ff34de4d3f4de24a2199428
Matthew Brown <me@matthewbrown.io>
2020-05-05 20:17:09 +0100

    Update site. Renable polling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.shipton-mill.com/queue"
+url = "https://www.shipton-mill.com/trade/bulk-shop"
 exact_match = "Sorry, we don't have any delivery slots available at this time, please come back later."
 
 response = requests.get(url)
@@ -11,8 +11,8 @@ soup = BeautifulSoup(response.text, "html.parser")
 sorry_messages = soup.body.find_all(text=exact_match)
 if len(sorry_messages) == 0:
     # There is no sorry message! Alert me!
-    print("SEND ALERT")
-    exit(1) # Exit with an error
+    print("SLOTS AVAILABLE")
+    exit(1)  # Exit with an error
 else:
-    print("NO ALERT")
-    exit(0) # Exit with no error code
+    print("NO SLOTS")
+    exit(0)  # Exit with no error code
